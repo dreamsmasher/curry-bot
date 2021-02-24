e167ec6fb5005b5e93dc52ba198cebd9371a0e78
Norman Liu <normanwuweiliu@gmail.com>
2021-02-24 10:27:59 -0800

    added newtype and instances, implemented core DB queries

--- src/DB.hs
+++ src/DB.hs
@@ -1,18 +1,27 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module DB where
 
-import Types
+import Control.Lens
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Reader
+import Data.Functor
 import Data.Profunctor.Product
-import Opaleye
+import Data.Text (Text, pack)
 import Data.Time (UTCTime)
+import Data.Tuple.Curry (Curry, uncurryN)
+import Database.PostgreSQL.Simple (Connection)
+import Opaleye
+import Types
 
 type ContrJoin a = Table a a
 
-type ProbFields = 
-    ( Field SqlInt4
-    , Field SqlText
-    , Field SqlText
-    , Field SqlText
-    )
+type ProbFields =
+  ( Field SqlInt4,
+    Field SqlText,
+    Field SqlText,
+    Field SqlTimestamptz
+  )
 
 type ProbTable = ContrJoin ProbFields
 
@@ -20,42 +29,84 @@ ccTable :: String -> TableFields writeFields viewFields -> Table writeFields vie
 ccTable = tableWithSchema "CCData"
 
 probTable :: ProbTable
-probTable = ccTable "Problems" $ p4 
-    ( tableField "id"
-    , tableField "name"
-    , tableField "description"
-    , tableField "submitted_at"
-    )
+probTable =
+  ccTable "Problems" $
+    p4
+      ( tableField "id",
+        tableField "name",
+        tableField "description",
+        tableField "submitted_at"
+      )
 
-type UserFields = 
-    ( Field SqlInt4
-    , Field SqlText
-    , Field SqlInt4
-    , Field SqlInt4
-    )
+type UserFields =
+  ( Field SqlInt4,
+    Field SqlText,
+    Field SqlInt4,
+    Field SqlInt4
+  )
 
 type UserTable = ContrJoin UserFields
 
 userTable :: UserTable
-userTable = ccTable "Users" $ p4 
-    ( tableField "id"
-    , tableField "discord_name"
-    , tableField "score"
-    , tableField "solved"
-    )
+userTable =
+  ccTable "Users" $
+    p4
+      ( tableField "id",
+        tableField "discord_name",
+        tableField "score",
+        tableField "solved"
+      )
 
-type InputFields = 
-    ( Field SqlInt4
-    , Field SqlJson
-    , Field SqlInt4
-    )
+type InputFields =
+  ( Field SqlInt4,
+    Field SqlJson,
+    Field SqlInt4
+  )
 
 type InputTable = ContrJoin InputFields
 
 inputTable :: InputTable
-inputTable = ccTable "Inputs" $ p3 
-    ( tableField "problem_id"
-    , tableField "json"
-    , tableField "answer"
-    )
+inputTable =
+  ccTable "Inputs" $
+    p3
+      ( tableField "problem_id",
+        tableField "json",
+        tableField "answer"
+      )
 
+_where :: Column SqlBool -> Select ()
+_where = viaLateral restrict
+
+-- some wacky typeclass stuff happening in the background, would be nice to have this be generic
+-- genericSel :: (c -> d) -> Select c -> DB [d]
+-- genericSel constr sel = (map constr <$>) . (`runSelect` sel)
+
+withConn :: (Connection -> IO b) -> DB b
+withConn f = do
+  conn <- ask
+  liftIO (f conn)
+
+problemSelect :: Select ProbFields -> DB [Problem]
+problemSelect ps = withConn $ (map (uncurryN Problem . over _1 ProbId) <$>) . (`runSelect` ps)
+
+userSelect :: Select UserFields -> DB [User]
+userSelect us = withConn $ (map (uncurryN User) <$>) . (`runSelect` us)
+
+getAllProblems :: DB [Problem]
+getAllProblems = problemSelect (selectTable probTable)
+
+matchId :: Column SqlInt4 -> ProbId -> Select ()
+matchId sqlId probId = _where (sqlId .== sqlInt4 (getId probId))
+
+getProbById :: ProbId -> DB [Problem]
+getProbById probId = problemSelect $ do
+  rows@(_probId, _, _, _) <- selectTable probTable
+  matchId _probId probId
+  pure rows
+
+getInputsById :: ProbId -> DB [Inputs]
+getInputsById probId = withConn $ (map (uncurry $ Inputs . pack) <$>) . (`runSelect` q)
+  where q = do
+          rows@(_probId, json, ans) <- selectTable inputTable
+          matchId _probId probId
+          pure (json, ans)
--- src/Types.hs
+++ src/Types.hs
@@ -1,14 +1,26 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, TemplateHaskell #-}
 
 module Types where
 
 import Data.Text ( Text )
 import Data.Time ( UTCTime )
+import Control.Monad.Trans.Reader
 import Control.Lens.TH
+import GHC.Generics
+import Database.PostgreSQL.Simple ( Connection )
 import Data.Aeson.TH
+    ( deriveJSON,
+      defaultOptions,
+      Options(fieldLabelModifier, constructorTagModifier) )
+import Data.Aeson (FromJSON, ToJSON (..), genericToEncoding)
+
+-- represents a function that takes a DB connection
+type DB = ReaderT Connection IO
+
+newtype ProbId = ProbId {getId :: Int} deriving (Eq, Show, Generic)
 
 data Problem = Problem
-    { _probId     :: Int
+    { _probId     :: ProbId
     , _probName   :: Text
     , _probDesc   :: Text
     , _probSubmit :: UTCTime
@@ -29,6 +41,11 @@ data Inputs = Inputs
 makeLenses ''Problem
 makeLenses ''User
 makeLenses ''Inputs
+
+-- using generics for automatic instances
+instance FromJSON ProbId where
+instance ToJSON ProbId where
+    toEncoding = genericToEncoding defaultOptions
 
 -- can't abstract these options into a common thing within the same module
 deriveJSON defaultOptions
