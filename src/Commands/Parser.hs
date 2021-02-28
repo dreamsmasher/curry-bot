51f1ba12594f2eb023d6f42c3987c3e6c88b3e18
Norman Liu <normanwuweiliu@gmail.com>
2021-02-27 19:21:46 -0800

    parsers almost ready, need to add JSON handlers for message attachments

--- src/Commands/Parser.hs
+++ src/Commands/Parser.hs
@@ -8,11 +8,14 @@ import Data.Text qualified as T
 import Data.Text (Text)
 import Data.Char (toLower, toUpper, chr, ord)
 import Commands.Types
+import Types
 import Data.Maybe
+import Data.Aeson
 
 import Commands.TH
 
 import Data.Functor
+import Discord.Internal.Types.Channel (Message, Attachment)
 import Control.Applicative (liftA2, empty)
 import Text.Read (readMaybe)
 import Data.Bits (xor)
@@ -41,4 +44,20 @@ parseCommand = do
     char msgPrefix
     s <- choice (map (try . caseBlindS) commandStrings) 
     maybe empty pure . readMaybe . capitalize $ s
-    
+
+parseProbId :: Parser ProbId
+parseProbId = ProbId . read <$> many1 digit 
+
+parseBotReq :: Parser BotReq
+parseBotReq = do
+    cmd <- parseCommand
+    case cmd of
+        Submit -> do
+            spaces
+            pid <- parseProbId
+            spaces
+            SubmitR pid . T.pack <$> manyTill anyChar eof
+        Get -> spaces >> parseProbId <&> GetR
+        New -> pure NewR
+        Input -> pure InputR
+        -- these two are expected to be passed in as JSON
--- src/Commands/Types.hs
+++ src/Commands/Types.hs
@@ -1,8 +1,19 @@
+{-# LANGUAGE GADTs #-}
 module Commands.Types where
 
+import Types
+import Data.Text ( Text )
 -- no camelcase here, 
 -- because the parsers depend on the string representations of these type constructors
 data BotCmd = Submit 
             | Get
             | New
+            | Input
             | Signup deriving (Eq, Show, Enum, Read)
+
+data BotReq where
+    SubmitR :: ProbId -> Text -> BotReq
+    GetR :: ProbId -> BotReq
+    NewR :: BotReq
+    InputR :: BotReq
+    deriving (Eq, Show)
