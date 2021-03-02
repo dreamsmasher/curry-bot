42fa75cda901e3984844b7b53ea3b577f2bcd40a
Norman Liu <normanwuweiliu@gmail.com>
2021-03-02 15:09:36 -0800

    solution checker works

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE OverloadedLists #-}
 module Utils where
 
+import Control.Applicative (liftA2, empty)
 import Data.Text (Text)
 import Data.Text qualified as T
+import Data.Text.Encoding (encodeUtf8)
 import Data.Maybe
 import Text.Read (readMaybe)
+import Data.ByteString.Lazy (fromStrict)
 import Discord.Internal.Types.User qualified as U
 import Types
 import Data.Aeson
@@ -19,14 +22,18 @@ tShow = T.pack . show
 
 compareSolutions :: JSONType -> Text -> Text -> Bool
 compareSolutions t sol ans = fromMaybe False $ do
-    pure True 
+    let toVal :: Text -> Maybe Value
+        toVal = decode . fromStrict . encodeUtf8
+    solVal <- toVal sol 
+    ansVal <- toVal ans
+    pure $ compareJSONType t ansVal && solVal == ansVal
 
 compareJSONType :: JSONType -> Value -> Bool
+compareJSONType (Arr a) (Array b) = all (compareJSONType a) b
 compareJSONType NumT (Number n) = True
 compareJSONType StrT (String s) = True
-compareJSONType (Arr a) (Array b) = all (compareJSONType a) b
+compareJSONType _ _ = False
 -- is all rly necessary here? 
 -- e.g. if you have Array [Number 1, String 'hello', etc], would lead to a false positive
-
-tst :: Value
-tst = Array [Array[Number 5]]
+-- we could probably use the Eq instance for Aeson.Value, but having the expected type stored with 
+-- the problem is also pretty useful for UX
